eae2e13266ac0b64112b3592aecffd5a1df3294c
Smartin Cherian <smartin.cherian@gmail.com>
2022-03-11 16:48:12 +0530

    11032022

--- index.js
+++ index.js
@@ -12,12 +12,25 @@ const ScheduleInfo = require('./src/model/ScheduleDB')
 //object init
 const app = express();
 app.use(cors());
-app.use(express.static('./public'));
+
+app.use(express.static(path.join(__dirname, 'src/build')));
+app.use(express.static('public'));
+app.use('/static', express.static('public'))
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const jwt = require('jsonwebtoken');
 const bcrpt = require('bcrypt');
 
+//photo upload
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public'),
+    filename: function (req, file, cb) {   
+        // null as first argument means no error
+        cb(null, Date.now() + '-' + file.originalname )  
+    }
+})
 
 app.get('/', function (req, res) {
     res.send("trainer server up");
@@ -383,9 +396,49 @@ app.post('/api/editprofile/:id/', async (req, res) => {
 
         })
 })
+
+//photoupload
+app.post('/api/photo/:id', async (req, res) => {	
+
+    const id = req.params.id;
+    try{
+        
+
+        let upload = multer({ storage: storage}).single('photo');
+       
+
+        upload(req, res, function(err) {
+            if (!req.file) {
+                return res.send('Please select an image to upload');
+            }
+            else if (err instanceof multer.MulterError) {
+                return res.send(err);
+            }
+            else if (err) {
+                return res.send(err);
+            }
+            const update = {
+				photo: req.file.path
+			};
+            const photo = req.file.filename;
+            // console.log(update);
+            console.log(photo);
+            console.log(id);
+            const filter = { id: id };
+            TrainerInfo.findOneAndUpdate(filter, {photo: photo}, { new: true })
+            .then(function(users){
+            res.json(users)
+    });
+        });  
+    }catch (err) {console.log(err)}
+    }); 
 //listening to port
 
 //app.listen(process.env.PORT || 5000, () => { console.log(`Listening on port ${process.env.PORT} or 5000`) });
 //listening to port
 
-app.listen(5000, () => { console.log("listening on port 5000") });
+app.get('*', (req,res)=> {
+    res.sendFile(path.join(__dirname + '/src/build/index.html'));
+})
+
+app.listen(process.env.PORT ||5000, () => { console.log("listening on port 5000") });
